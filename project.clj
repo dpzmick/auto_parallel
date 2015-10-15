04464e5277407981d1702b8d6b03dfdfdac6d03e
dpzmick <dpzmick@gmail.com>
2015-10-14 20:05:18 -0500

    commit to save sort of working nested lets

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [
+  :dependencies [[org.clojure/tools.trace "0.7.9"]
                  [org.clojure/clojure "1.7.0"]
                  [com.climate/claypoole "1.1.0"]]
   :main ^:skip-aot auto-parallel.core
--- src/auto_parallel/core.clj
+++ src/auto_parallel/core.clj
@@ -1,13 +1,80 @@
 (ns auto-parallel.core
-  (:gen-class))
+  (use [clojure.tools.trace]))
 
 ;; make it easier to play with syntax tree
 (defn args [expr] (rest expr))
 (defn fun  [expr] (first expr))
-(defn const? [expr] (not (list? expr)))
-(defn all-args-constant?
+(defn const? [expr] (not (seq? expr)))
+(defn all-args-const?
   [expr]
   (every? true? (map const? (args expr))))
+
+;; everything this recovers in :names can be computed in parallel
+;; call this multiple times to get multiple parallelize able steps
+;; assumes everything is a function call
+(defn prune
+  "prunes a single level of leaves from syntax tree"
+  [expr]
+  (if (or (const? expr) (all-args-const? expr))
+    (let
+      [n (gensym "expr")]
+      {:expr n, :names {n expr}})
+
+    (let
+      [subresults (map prune (args expr))
+       subexprs   (map :expr subresults)
+       subnames   (map :names subresults)
+       mynames    (apply merge subnames)]
+      {:expr (cons (fun expr) subexprs), :names mynames})))
+
+;; TODO all these interfaces are getting wacky
+;; outputs an expr and a names-list
+;; each element in the names-list is a map, whose bindings can be computed in
+;; parallel
+(defn prune-all-levels
+  ([expr] (prune-all-levels expr (list)))
+
+  ([expr names-list]
+   (let
+     [{e :expr n :names} (prune expr)]
+     (if (const? e)
+       {:expr e :names-list (cons n names-list)}
+       (recur e (cons n names-list))))))
+
+(defn make-nested-lets
+  ([{expr :expr names-list :names-list}] (make-nested-lets expr names-list))
+  ([expr names-list]
+   (if (empty? names-list)
+     expr
+
+     (let
+       [bindings (apply vector
+                        (apply concat
+                               (into (list) (first names-list))))]
+       (recur
+         `(let ~bindings ~expr)
+         (rest names-list))))))
+
+(defn futurize
+  [expr]
+  (if (const? expr)
+    (list 'future expr)
+
+    (list
+      'future
+      (cons
+        (fun expr)
+        (map
+          #(if (.startsWith (.toString %) "expr")
+             (list 'deref %)
+             %)
+          (args expr))))))
+
+(defn pruned-hack
+  [{e :expr n :names-list}]
+  {:expr (list 'deref e) :names-list (map futurize-names-map n)})
+
+(defmacro par2 [expr] (make-nested-lets (pruned-hack (prune-all-levels expr))))
 
 (defn naive-parallel
   [expr]
