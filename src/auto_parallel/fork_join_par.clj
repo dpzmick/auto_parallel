60bb6eaa6d012f90d56facc122c13087a4b9ae6a
David Zmick <dpzmick@gmail.com>
2015-12-03 13:36:45 -0600

    removed fj versions of builtin functions (they won't be used)

--- src/auto_parallel/fork_join_par.clj
+++ src/auto_parallel/fork_join_par.clj
@@ -13,25 +13,3 @@
 (defn fork [t] (.fork t))
 (defn join [t] (.join t))
 (defn compute [t] (.compute t))
-
-(defn pmap
-  "
-  Sort of confusing behavior.
-  Starts a ForkJoin task for each element in the sequence, waits for the first
-  to finish, then returns. The rest will be computed lazily in the background.
-  "
-  [f lst]
-  (let
-    [exprs  (map #(fn [] (f %)) lst)
-     tasks  (map new-task exprs)
-     forked (doall (map fork (rest tasks))) ;; need to force them all to start
-     me     (compute (first tasks))]
-    (cons me (map join forked)))) ;; because this map is lazy
-
-(defn pcalls
-  [& fs]
-  (pmap #(%) fs))
-
-(defmacro pvalues
-  [& exprs]
-  `(pcalls ~@(for [e exprs] `(fn [] ~e))))
