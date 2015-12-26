c82f9561ff9e4cc88481a68eef0ba8f3338c5c2e
David Zmick <dpzmick@gmail.com>
2015-12-26 09:57:50 -0600

    asd

--- src/auto_parallel/ast_crawl.clj
+++ src/auto_parallel/ast_crawl.clj
@@ -1,22 +1,27 @@
 (ns auto-parallel.ast-crawl)
 
-(defn ast-crawl-expr [expr callbacks callback-args]
-  "
-  Crawls the AST for a given expression, calling the callback functions with
-  callback-args on each subexpression.
+;; TODO map syntax
+(defn ast-crawl-expr
+  ([expr callbacks] (ast-crawl-expr expr callbacks nil))
+  ([expr callbacks callback-args]
+   "
+   Crawls the AST for a given expression, calling the callback functions with
+   callback-args on each subexpression.
 
-  callbacks:
-  -
-  "
-  (if (sequential? expr)
-    ;; we have a function call or some other form (like a vector or list
-    ;; literal)
-    (cond
-      (= 'let* (first expr)) ((:let-cb callbacks) expr callback-args)
-      (vector? expr)         ((:vector-cb callbacks) expr callback-args)
-      (seq? expr)            ((:sequential-cb callbacks) expr callback-args)
-      :else                  (throw (Exception. "this form is not supported")))
+   callbacks:
+   - let-cb
+   - vector-cb
+   - sequential-cb
+   - const-cb
+   "
+   (if (sequential? expr)
+     ;; we have a function call or some other form (like a vector or list
+     ;; literal)
+     (cond
+       (= 'let* (first expr)) ((:let-cb callbacks) expr callback-args)
+       (vector? expr)         ((:vector-cb callbacks) expr callback-args)
+       (seq? expr)            ((:sequential-cb callbacks) expr callback-args)
+       :else                  (throw (Exception. "this form is not supported")))
 
-    ;; the expression is a single term
-    ((:const-cb callbacks) expr callback-args)
-   ))
+     ;; the expression is a single term
+     ((:const-cb callbacks) expr callback-args))))
