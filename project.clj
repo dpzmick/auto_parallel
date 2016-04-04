0080d012659e6a9d68f3ea349d41b2684148d4e3
David Zmick <dpzmick@gmail.com>
2016-04-03 22:50:46 -0500

    be less noisy

--- project.clj
+++ project.clj
@@ -13,7 +13,7 @@
             "jbenchmark" ["with-profile" "production" "run" "-m" "com.dpzmick.auto_parallel_java.App"]
             "slamhound" ["run" "-m" "slam.hound"]}
 
-  :jvm-opts ["-server" "-Xms5G" "-Xmx5G" "-verbose:gc"]
+  :jvm-opts ["-server" "-Xms5G" "-Xmx5G"] ;"-verbose:gc"]
 
   :java-source-paths ["src_java/"]
 
